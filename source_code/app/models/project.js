d1928ca4569e8ab38325bcf5b7be97769f9a5f2f
Chris N <cnara004@fiu.edu>
2015-10-05 16:09:23 -0400

    cleaner way to deal with routing

---
+++ source_code/app/models/project.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Chris on 9/30/2015.
+ *
+ * model for collected project info
+ */
+var mongoose    = require('mongoose');
+var schema      = mongoose.Schema;
+var shortid     = require('shortid');
+
+var Project = new schema({
+    proj: {type: String, required: true},
+    user: {type: String, required: true},
+    email: {type: String, required: true},
+    disc: {type: String, required: true},
+    request: {type: Number, required: true, min: 1},
+    max: {type: Number, required: true},
+    status: {type: Boolean, default: false},
+    _id: {type: String, index: {unique: true}, default: shortid.generate()}
+    //I need to add some project id, would be easier to track/manage by unique ID number.
+});
+
+module.exports = mongoose.model('Project', Project);
