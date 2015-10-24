7557ae4adfb07e221cd70d17050811355685a45c
egonz077 <egonz077@fiu.edu>
2015-10-24 10:30:23 -0400

    egonz077 show and tell code 10-21

---
+++ egonz077/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'port': process.env.PORT || 8080,
+	'database': 'root:1234@localhost:27017/admin',
+	'secret': 'ilovebigbootyhoes'
+};
---
+++ egonz077/public/assets/js/app.js
@@ -0,0 +1,11 @@
+angular.module('mainApp', ['appRoutes', 'userCtrl', 'userService', 'angular.filter'])
+
+
+
+    .controller('mainController', function() {
+
+        var vm = this;
+
+
+
+    });
