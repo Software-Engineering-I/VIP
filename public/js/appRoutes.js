9466ecf3a757c6185fea65a1b75e269726d18a84
Steven <mrowe122@gmail.com>
2015-10-29 20:57:07 -0400

    reoganized file structure

--- public/js/appRoutes.js
+++ public/js/appRoutes.js
@@ -5,28 +5,24 @@ angular.module('appRoutes', ['ngRoute'])
 
 		// home page
 		.when('/', {
-			templateUrl: 'app/views/pages/home.html',
+			templateUrl: 'views/pages/home.html',
 			controller: 'homeController',
 			controllerAs: 'home'
 		})
 
+		//about page
 		.when('/about', {
-			templateUrl: 'app/views/pages/about.html',
+			templateUrl: 'views/pages/about.html',
 			controller: 'aboutController',
 			controllerAs: 'about'
 		})
 
+		//programs page
 		.when('/programs', {
-			templateUrl: 'app/views/pages/programs.html',
+			templateUrl: 'views/pages/programs.html',
 			controller: 'programsController',
 			controllerAs: 'programs'	
 		})
-
-		.when('/report', {
-			templateUrl: 'app/views/pages/report.html',
-			controller: 'programsController',
-			controllerAs: 'programs'
-		});
 
 	$locationProvider.html5Mode(true);
 
--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 // modules =================================================
-var express        = require('express');
-var app            = express();
-var mongoose       = require('mongoose');
-var bodyParser     = require('body-parser');
-var methodOverride = require('method-override');
-var path		   = require('path');
+var express			= require('express');
+var app				= express();
+var mongoose		= require('mongoose');
+var bodyParser		= require('body-parser');
+var methodOverride	= require('method-override');
+var path			= require('path');
+var config			= require('./config');
 
 // configuration =	==========================================
-	
-// config files
-var db = require('./config/db');
 
 var port = process.env.PORT || 3000; // set our port
 // mongoose.connect(db.url); // connect to our mongoDB database (commented out after you enter in your own credentials)
@@ -18,9 +16,9 @@ app.use(express.static(__dirname + '/public')); // set the static files location
 
 // set up our one route to the index.html file
 app.get('*', function(req, res) {
-	res.sendFile(path.join(__dirname + '/public/app/views/index.html'));
+	res.sendFile(path.join(__dirname + '/public/views/index.html'));
 });
 
 // start app ===============================================
 app.listen(port);	
-console.log('Magic happens on port ' + port); 			// shoutout to the user
+console.log('Magic happens on port ' + port);
