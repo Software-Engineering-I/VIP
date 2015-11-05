7e9d456f12b3ef9c693107ba78050076cb08bc4c
lborg019@fiu.edu <Lukas>
2015-11-05 21:19:59 +0000

    new nav bar... bootstrap & css issue fixed, need to integrate user schema with login schema then ready to integrate

--- code/app/routes/routes.js
+++ code/app/routes/routes.js
@@ -6,4 +6,18 @@ module.exports = function(app) {
 
 	// frontend routes =========================================================
 	// route to handle all angular requests
+	/* new route 
+	app.set('appPath', 'public');
+	app.use(express.static(__dirname + '/public'));
+
+	app.route('/*')
+	  .get(function(req, res){
+	    res.sendfile(app.get('appPath') + '/index.html');
+	});*/
+
+	//old app route
+	app.get('*', function(req, res) {
+		res.sendfile('./public/views/index.html');
+	});
+
 };
--- code/public/js/app.js
+++ code/public/js/app.js
@@ -1,33 +1,93 @@
-angular.module('mainApp', ['appRoutes'])
+var MyApp = angular.module('MyApp', ['ui.router', 'satellizer'])
+	.config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider){
+		$urlRouterProvider.otherwise('/home');
+	
+		$stateProvider
+		.state('signin', {
+			url:'/signin',
+			templateUrl: "views/pages/signin.html",
+			controller: "SignInCtrl"
+		})
+		.state('signout', {
+			url:'/signout',
+			templateUrl: "views/pages/signout.html",
+			controller: "SignOutCtrl"
+		})
+		.state('register', {
+			url:'/register',
+			templateUrl:"views/pages/register.html",
+		})
+		.state('home', {
+			 url:'/home',
+			 templateUrl: "/views/pages/home.html"
+		 })
+		.state('about',{
+			 url:'/about',
+			 templateUrl: "/views/pages/about.html"
+		})
+		.state('programs',{
+			url:'/programs',
+			templateUrl: "/views/pages/programs.html"
+		})
+		.state('profile',{
+			url: '/profile',
+			templateUrl: "views/pages/profile.html"
+		})
+	}]);
 
-.controller('mainController', function() {
+MyApp.config(function($authProvider) {
 
-	var vm = this;
+    $authProvider.google({
+      clientId: '577160044779-hjsgal811fa0jlmd6jrl9lv0ab17hfgs.apps.googleusercontent.com'
+    });
 
-	vm.tagline = 'To the moon and back!';	
-
-})
-
-.controller('homeController', function() {
-
-	var vm = this;
-
-	vm.tagline = 'To the moon and back!';	
-
-})
-
-
-.controller('aboutController', function() {
-
-	var vm = this;
-
-	vm.tagline = 'Nothing beats a pocket protector!';
-
-})
-
-.controller('programsController', function() {
-
-	var vm = this;
-	vm.tagline = 'The square root of life is pi!';	
+    $authProvider.live({
+      clientId: 'Microsoft Client ID'
+    });
 
 });
+
+MyApp.controller('SignInCtrl', function($scope, $location, $auth)
+{
+	$scope.authenticate = function(provider)
+	{
+		$auth.authenticate(provider);
+		$location.path('#/signin');
+	};
+	/*.catch(function(response){
+		$location.path('#/error');
+	});*/
+});
+
+MyApp.controller('SignOutCtrl', function($scope, $auth){
+	if(!$auth.isAuthenticated()) { return; }
+	$auth.logout();
+});
+
+MyApp.controller('NavbarCtrl', function($scope, $auth) {
+    $scope.isAuthenticated = function() {
+      return $auth.isAuthenticated();
+    };
+  });
+
+MyApp.controller('ProfileCtrl', function($scope, $auth, Account) {
+	$scope.getProfile = function() {
+		Account.getProfile()
+		  .then(function(response) {
+		    $scope.user = response.data;
+		  });
+	};
+
+	$scope.getProfile();
+});
+
+MyApp.factory('Account', function($http) {
+    return {
+      getProfile: function() {
+        return $http.get('/api/me');
+      },
+      updateProfile: function(profileData) {
+        return $http.put('/api/me', profileData);
+      }
+    };
+  });
