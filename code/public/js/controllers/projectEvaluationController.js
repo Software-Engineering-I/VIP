4d2db2588e39587d0a58995327497a79a0f36cf6
lmend066 <lmend066@fiu.edu>
2015-12-02 13:45:58 -0500

    Project Evaluation Updated

--- code/public/js/controllers/projectEvaluationController.js
+++ code/public/js/controllers/projectEvaluationController.js
@@ -1,22 +1,34 @@
-angular.module('projectEvaluationControl', ['projectEvaluationService'])
+angular.module('projectEvaluationControl', ['projectEvaluationService', 'userService'])
 
-// controller applied to user creation page
-    .controller('projectEvaluationController', function(Evaluation) {
+    // controller applied to user creation page
+    .controller('projectEvaluationController', function(Evaluations, User) {
 
         var vm = this;
         vm.title = "Project Evaluation";
+
+        // grab all the users at page load
+        // "User" refers to userService factory object
+        User.all()
+            .success(function(data) {
+                // when all the users come back, remove the processing variable
+                vm.processing = false;
+                // bind the users that come back to vm.users
+                vm.users = data;
+            });
 
         // function to create a user
         vm.saveEvaluation = function() {
             vm.processing = true;
             vm.message = '';
 
-            // use the create function in the userService
-            Evaluation.create(vm.feedback)
+            // use the create function
+            Evaluations.create(vm.feedback)
                 .success(function(data) {
                     vm.processing = false;
                     vm.feedback = {};
                     vm.message = data.message;
                 });
         };
+
+
     });
